2329a03f83f87c293936c869aa398ef1aaef19fb
mohit_compunnel <mohit.saxena@compunnel.in>
2019-05-24 15:49:18 +0530

    implement mongo db connection

--- server.js
+++ server.js
@@ -2,8 +2,27 @@ const express = require('express');
 const port = 3000;
 const app = express();
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const providerRoute = require('./app/routes/provider.route');
+
+let dev_db_url = 'mongodb+srv://admin:bbFCj2XeUf1zD2fG@godb-tsqac.mongodb.net/test?retryWrites=true';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true }, (response)=>{
+        console.log(response);
+});
+//mongoose.Promise = global.Promise;
+
+let db = mongoose.connection;
+db.on('error', (err)=>{
+    console.error('Mongo DB connection failed', err)
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
 
 app.use('/providers', providerRoute);
 
